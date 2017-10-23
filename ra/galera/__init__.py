5cc22e429463750c85eb77611d39c13aa74d519e
Damien Ciabrini <dciabrin@redhat.com>
2017-10-23 11:14:07 +0200

    Temporary disable tests for unmanaged state

--- ra/galera/__init__.py
+++ ra/galera/__init__.py
@@ -2,11 +2,11 @@ from .galerascenarios import scenarios
 import galeratests
 import galeratests_node
 import galeratests_sst
-import galeratests_unmanaged
+# import galeratests_unmanaged
 
 __all__ = [ "tests", "scenarios", "name" ]
 tests = galeratests.tests + \
         galeratests_node.tests + \
-        galeratests_sst.tests + \
-        galeratests_unmanaged.tests
+        galeratests_sst.tests # + \
+        # galeratests_unmanaged.tests
 name = "Galera"
