cb4fa5d0f5f02d6e4129f600fb485e06f39f7fb7
Damien Ciabrini <dciabrin@redhat.com>
2018-08-07 22:05:29 +0200

    Add column layout to tile windows

--- ra-tester-tmux.sh
+++ ra-tester-tmux.sh
@@ -19,22 +19,38 @@
 VM_NAME_PREFIX=
 SESSION=
 SSH_ARGS=
+ROWS=3
+COLUMNS=1
+
+pane_to_node()
+{
+    local pane=$1
+    local panezero row col pos_in_col node
+
+    panezero=$((pane-1))
+    row=$((panezero/COLUMNS))
+    col=$((panezero%COLUMNS))
+    pos_in_col=$(((ROWS*col)+row))
+    node=$((pos_in_col+1))
+    echo $node
+}
 
 check_ssh()
 {
     local location=$1
-    local i=1
+    local node
     tmux set-window-option -t $SESSION:$location synchronize-panes off
-    tmux list-pane -F '#D:#{pane_pid}' -t $SESSION:$location | while IFS=: read pane pid
+    tmux list-pane -F '#D:#{pane_pid}:#P' -t $SESSION:$location | while IFS=: read pane pid idx
     do
         # if pid is a shell and it has no children, we can ssh into the target node
         if cat /proc/$pid/cmdline | grep -qe '-.*sh'; then
-            if ! (ps axfo ppid | grep -q $pid); then
-                echo "ssh ${SSH_ARGS} ${VM_NAME_PREFIX}$i" in $location - $pane
-                tmux send-key -t $pane "ssh ${SSH_ARGS} ${VM_NAME_PREFIX}$i" Enter
+            children=$(ps --forest -g $pid)
+            if ! $(echo $children | grep -q ssh); then
+                node=$(pane_to_node $idx)
+                # echo "ssh ${SSH_ARGS} ${VM_NAME_PREFIX}$node" in $location - $pane
+                tmux send-key -t $pane "ssh ${SSH_ARGS} ${VM_NAME_PREFIX}$node" Enter
             fi
         fi
-        ((i++))
     done
 }
 
@@ -42,25 +58,38 @@ create_window()
 {
     local name=$1
     local location=$2
+    local layout
+
     tmux new-window -dk -n $name -t $SESSION:$location
-    tmux split-window -v -t $SESSION:$location
-    tmux split-window -v -t $SESSION:$location
-    tmux select-layout -t $SESSION:$location even-vertical
+
+    for ir in `seq $((ROWS-1))`; do
+         tmux split-window -v -t $SESSION:$location
+    done
+
+    for c in `seq $((COLUMNS-1))`; do
+        for r in `seq $ROWS -1 1`; do
+            tmux split-window -h -t $SESSION:$location.$r
+        done
+    done
+
+    if [ $COLUMNS -gt 1 ]; then
+        layout=tiled
+    else
+        layout=even-vertical
+    fi
+
+    tmux select-layout -t $SESSION:$location $layout
 }
 
 # force create a ssh control master.
 # controlpersist makes the magic work
 ensure_control_master()
 {
-    ssh ${SSH_ARGS} ${VM_NAME_PREFIX}1 true&
-    local pid1=$!
-    ssh ${SSH_ARGS} ${VM_NAME_PREFIX}2 true&
-    local pid2=$!
-    ssh ${SSH_ARGS} ${VM_NAME_PREFIX}3 true&
-    local pid3=$!
-    wait $pid1
-    wait $pid2
-    wait $pid3
+    for i in `seq $((ROW*COLUMNS))`; do
+        ssh ${SSH_ARGS} ${VM_NAME_PREFIX}1 true&
+        local pid=$!
+        wait $pid
+    done
 }
 
 
@@ -71,8 +100,10 @@ Create a tmux session for monitoring cluster nodes
 Usage: $(basename $0) VM_NAME_PREFIX [options ...]
 
 Options:
-   --ssh CONFIG    Use config file CONFIG to ssh to cluster nodes
-   --force         Re-create the tmux session if it already exists
+   --ssh CONFIG   Use config file CONFIG to ssh to cluster nodes
+   --rows R       Split tmux sessions in R rows, laid out vertically
+   --columns C    Split tmux sessions in R rows and C columns, tiled
+   --force        Re-create the tmux session if it already exists
 
 Exemple:
    # Connect to cluster nodes centos1,centos2,centos3
@@ -85,6 +116,8 @@ CREATE=0
 while [ "$1" != "" ]; do
     case "$1" in
         --ssh ) SSH_ARGS="-F$2"; shift 2;;
+        --rows ) ROWS="$2"; shift 2;;
+        --columns ) COLUMNS="$2"; shift 2;;
         --force ) CREATE=1; shift;;
         -h|--help ) usage; exit 0;;
         * )
@@ -99,6 +132,12 @@ while [ "$1" != "" ]; do
             ;;
     esac
 done
+
+if [ "x$VM_NAME_PREFIX" = "x" ]; then
+    echo "ERROR: missing parameter VM_NAME_PREFIX" >&2
+    usage
+    exit 1
+fi
 
 SESSION=$VM_NAME_PREFIX-ra
 
