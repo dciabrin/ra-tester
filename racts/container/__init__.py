e442598e725a857adcf381cd322dfa0d14683c5b
Damien Ciabrini <dciabrin@redhat.com>
2019-03-21 22:51:15 +0100

    Fix container engine detection, default hints for RHEL-based OS

--- racts/container/__init__.py
+++ racts/container/__init__.py
@@ -1,5 +1,6 @@
-from . import engine
+from cts.remote import RemoteFactory
 
+from . import engine
 from .docker import Docker
 from .podman import Podman
 
@@ -18,5 +19,17 @@ def get_container_engine(env):
     return engines[env["container_engine"]](env)
 
 def autodetect_container_engine(env):
-    engine = next(e for e in priority if engines[e](env).is_detected())
-    return engine
+    try:
+        engine = next(e for e in priority if engines[e](env).is_detected())
+        return engine
+    except StopIteration:
+        # RHEL 8 uses podman instead of docker
+        rsh = RemoteFactory().getInstance()
+        node = env["nodes"][0]
+        inspect = rsh(node, "/usr/bin/hostnamectl | grep CPE", stdout=True).rstrip()
+        if inspect:
+            items=inspect.split(":")
+            # ['       CPE OS Name', ' cpe', '/o', 'redhat', 'enterprise_linux', '8.0', 'GA']
+            if items[5].startswith('8.'):
+                return "podman"
+        return "docker"
