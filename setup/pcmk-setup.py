e23deed56cb6e384064b1f4efc29dcfd144fe76e
Damien Ciabrini <dciabrin@redhat.com>
2019-07-31 08:55:16 +0200

    New setup based on pipenv, custom cts install, and python3

---
+++ setup/pcmk-setup.py
@@ -0,0 +1,37 @@
+import setuptools.command.build_py
+from setuptools import setup
+import glob
+
+class BuildPyCommand(setuptools.command.build_py.build_py):
+    def run(self):
+        rules = {
+            '@PYTHON@': '/usr/bin/env python',
+            '@BASH_PATH@': '/usr/bin/env bash',
+            '@datadir@': '/usr/share',
+            '@PACKAGE@': 'pacemaker',
+            '@CRM_CONFIG_DIR@': '/var/lib/pacemaker/cib',
+            '@CRM_LOG_DIR@': '/var/log/pacemaker',
+            '@CRM_DAEMON_USER@': 'hacluster',
+            '@CRM_DAEMON_DIR@': '/usr/libexec/pacemaker',
+            '@OCF_ROOT_DIR@': '/usr/lib/ocf'
+        }
+        for i in glob.glob("cts/*.py.in"):
+            with open(i, 'r') as inf, open(i[:-3], 'w') as outf:
+                for l in inf.readlines():
+                    for k,v in rules.items():
+                        l = l.replace(k, v)
+                    outf.write(l)
+        setuptools.command.build_py.build_py.run(self)
+
+
+setup(name = 'cts',
+      version = '0.1',
+      description = 'Pacemaker Cluster Test Suite',
+      url = 'http://github.com/ClusterLabs/pacemaker',
+      author = 'Pacemaker project contributors',
+      license = 'GPLv2',
+      packages = ['cts'],
+      cmdclass = {
+          'build_py': BuildPyCommand,
+      },
+      zip_safe = False)
