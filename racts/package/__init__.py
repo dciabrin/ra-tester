e7de558edc2704cd2f993e49ea74de864749a4d5
Damien Ciabrini <dciabrin@redhat.com>
2020-01-24 17:18:43 -0500

    New package installer, with name mapping per distro

---
+++ racts/package/__init__.py
@@ -0,0 +1,26 @@
+import re
+
+from cts.remote import RemoteFactory
+
+from . import manager
+from .rpm import Rpm
+
+managers = {
+        "rpm": Rpm
+}
+
+def get_package_manager(env):
+    # TODO: check whether we can create an instance instead of
+    # doing a relying on a singleton
+    return env["package_manager"]
+
+def autodetect_package_manager(env):
+    rsh = RemoteFactory().getInstance()
+    distrib = rsh(env["nodes"][0], "lsb_release -i", stdout=1)
+    distro_flavor = ""
+    manager = False
+    if re.search("RedHatEnterpriseServer", distrib):
+        manager = managers["rpm"](env, "rpm", "rhel")
+
+    assert manager is not False
+    return manager
---
+++ racts/package/manager.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+import yaml
+
+class PackageManager(ABC):
+    def __init__(self, env, pkg_format, flavor):
+        self.Env = env
+        with open(env["package_mapping"], "r") as f:
+            self.mapping = yaml.safe_load(f)
+        self.pkg_format = pkg_format
+        self.flavor = flavor
+
+    @abstractmethod
+    def _is_installed(self, node, pkg):
+        pass
+
+    @abstractmethod
+    def _install(self, node, pkg):
+        pass
+
+    @abstractmethod
+    def _can_be_updated(self, node, pkg):
+        pass
+
+    @abstractmethod
+    def _update(self, node, pkg):
+        pass
+
+    def map_package_name(self, pkg):
+        ns = self.pkg_format + "-" + self.flavor
+        table = self.mapping.get(ns, {})
+        mapped = table.get(pkg, False)
+        if not mapped:
+            ns = self.pkg_format
+            table = self.mapping.get(ns, {})
+        r = table.get(pkg, pkg)
+        return r
+
+    def is_installed(self, node, pkg):
+        self._is_installed(node, self.map_package_name(pkg))
+
+    def install(self, node, pkg):
+        self._install(node, self.map_package_name(pkg))
+
+    def can_be_updated(self, node, pkg):
+        self._can_be_updated(node, self.map_package_name(pkg))
+
+    def update(self, node, pkg):
+        self._update(node, self.map_package_name(pkg))
