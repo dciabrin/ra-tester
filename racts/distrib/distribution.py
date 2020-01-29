a984dfbf3ab28e083b3ddab4005bb62b317e9f46
Damien Ciabrini <dciabrin@redhat.com>
2020-01-29 05:29:55 -0500

    new distrib class: combination of container + cluster + package manager

---
+++ racts/distrib/distribution.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+import yaml
+
+class Distribution(ABC):
+    def __init__(self, env):
+        self.Env = env
+
+    @abstractmethod
+    def cluster_manager(self):
+        pass
+
+    @abstractmethod
+    def package_manager(self):
+        pass
+
+    @abstractmethod
+    def container_engine(self):
+        pass
+
+    @abstractmethod
+    def add_insecure_container_registry(self, node, uri):
+        pass
