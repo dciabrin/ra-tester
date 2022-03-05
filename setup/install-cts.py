b067f3eb8c00ed568dbc1247fb8b22d5ab9210d0
Damien Ciabrini <dciabrin@redhat.com>
2022-03-05 09:36:22 +0000

    Fix installation of cts modules from git's main branch

--- setup/install-cts.py
+++ setup/install-cts.py
@@ -8,8 +8,8 @@ import sys
 import subprocess
 
 with tempfile.TemporaryDirectory(prefix='ratester') as tmpd:
-    url = 'https://github.com/ClusterLabs/pacemaker/archive/master.zip'
-    pcmk_zip = os.path.join(tmpd, 'pacemaker-master.zip')
+    url = 'https://github.com/ClusterLabs/pacemaker/archive/main.zip'
+    pcmk_zip = os.path.join(tmpd, 'pacemaker-main.zip')
 
     print("Downloading CTS from latest pacemaker snapshot (%s)" % url)
     urllib.request.urlretrieve(url, pcmk_zip)
@@ -17,7 +17,7 @@ with tempfile.TemporaryDirectory(prefix='ratester') as tmpd:
     print("CTS archive saved at %s. Make it installable by pip" % pcmk_zip)
     setup_py = os.path.abspath(os.path.join(__file__,'..','pcmk-setup.py'))
     with zipfile.ZipFile(pcmk_zip,'a') as z:
-        z.write(setup_py, 'pacemaker-master/setup.py')
+        z.write(setup_py, 'pacemaker-main/setup.py')
 
     pip_cmd = [sys.executable, '-m', 'pip', 'install', pcmk_zip]
     print("Installing CTS with \"%s\"" % " ".join(pip_cmd))
